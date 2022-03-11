7d6da4a8ad1acb19add0cc6eef8e5deaa5fb3c34
Alberto Moura <luan.ribeiro.dev@gmail.com>
2022-03-11 11:25:30 -0300

    Section 1 - desktop finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import './styles/styles.scss';
-import Section from './components/Section';
+import React from "react";
+import "./styles/styles.scss";
+import Section from "./components/Section";
 
 const App: React.FC = () => {
   return (
     <div className="app">
       <Section>
-        <h1 style={{position: 'absolute', left: 0, top: 0}}>Section 1</h1>
+        <h1 style={{position: "absolute", left: 0, top: 0}}>Section 1</h1>
         <figure className="green-circle"></figure>
 
         <div className="row overlay-row h-100">
+          <div id="top-bar">
+            <i className="fas fa-code" id="logo"></i>
+            <i className="fas fa-bars" id="menu-bar-button" title="Open navigation menu"></i>
+          </div>
           <div className="col-12 col-sm-6 center-div">
-            <div className="info-div">
-              <h1 className="header-text">Fullstack Developer<figure className="green-square" /></h1>
-              <p className="info-paragraph">
-                Junior web developer with <span className='green-text'>+3 years of programming</span> experience with companies and teams
+            <div id="section-1-info-div">
+              <h1>Fullstack Developer<figure className="green-square" /></h1>
+              <p>
+                Junior web developer with <span className="green-text">+3 years of programming</span> experience with companies and teams
                 {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in sem at dui interdum <span className="green-text">suscipit</span> et sit amet tortor. */}
               </p>
             </div>
@@ -25,7 +29,7 @@ const App: React.FC = () => {
                   <i className="fas fa-phone-alt"></i>
                 </a>
                 <a href="tel: +55 11 9 81389495">
-                  <span className='ms-2'>+55 11 981389495</span>
+                  <span className="ms-2">+55 11 981389495</span>
                 </a>
               </div>
 
@@ -34,11 +38,11 @@ const App: React.FC = () => {
                   <i className="fas fa-envelope"></i>
                 </a>
                 <a href="mailto:contact@luan-dev.com">
-                  <span className='ms-2'>contact@luan-dev.com</span>
+                  <span className="ms-2">contact@luan-dev.com</span>
                 </a>
               </div>
 
-              <span className='mt-3'>Luan Dev - All Rights Reserved</span>
+              <span className="mt-3">Luan Dev - All Rights Reserved</span>
             </div>
           </div>
           <div className="col-12 col-sm-6 center-div position-relative" id="self-div">
@@ -49,22 +53,22 @@ const App: React.FC = () => {
         </div>
       </Section>
       <Section>
-        <h1 style={{position: 'absolute', left: 0, top: 0}}>Section 2</h1>
+        <h1 style={{position: "absolute", left: 0, top: 0}}>Section 2</h1>
       </Section>
       <Section>
-        <h1 style={{position: 'absolute', left: 0, top: 0}}>Section 3</h1>
+        <h1 style={{position: "absolute", left: 0, top: 0}}>Section 3</h1>
       </Section>
       <Section>
-        <h1 style={{position: 'absolute', left: 0, top: 0}}>Section 4</h1>
+        <h1 style={{position: "absolute", left: 0, top: 0}}>Section 4</h1>
       </Section>
       <Section>
-        <h1 style={{position: 'absolute', left: 0, top: 0}}>Section 5</h1>
+        <h1 style={{position: "absolute", left: 0, top: 0}}>Section 5</h1>
       </Section>
       <Section>
-        <h1 style={{position: 'absolute', left: 0, top: 0}}>Section 6</h1>
+        <h1 style={{position: "absolute", left: 0, top: 0}}>Section 6</h1>
       </Section>
       <Section>
-        <h1 style={{position: 'absolute', left: 0, top: 0}}>Section 7</h1>
+        <h1 style={{position: "absolute", left: 0, top: 0}}>Section 7</h1>
       </Section>
     </div>
   );
